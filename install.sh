5f06fbcf8cc93f8981902965e4c02ba70a393e52
Christophe Parent <christopheparent@thatspaceandtime.org>
2018-03-25 12:09:10 -0700

    Update install file

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for d in "logs" "data/scraping" "data/matching"; do
+for d in "logs" "data/scraping" "data/matching" "web/data" "web/external"; do
     if [[ ! -d "$d" ]]; then
         mkdir -p "$d"
     fi
